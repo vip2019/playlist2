db394b101eb07473be81f28973c5a66916ef849b
vip2019 <42376185+vip2019@users.noreply.github.com>
2018-10-27 22:08:25 +0300

    Update name.js

--- name.js
+++ name.js
@@ -1,266 +1,235 @@
-//!!!!!!!!!!!!!!!!!!!/!/!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!!!!!/!/!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//  Built by Addison Rodomista
-//  Insta:  @rodomeista
-//!!!!!!!!!!!!!!!!/!/!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!!/!/!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ // HTML5 audio player + playlist controls
+var jsPlayer = document.querySelector('.player-wrap');
+if (jsPlayer) {
+	jsPlayer = {
+		wrap: jsPlayer,
+		player: jsPlayer.querySelector('audio'),
+		wrapList: (document.querySelector('.playlist-wrap') || {}),
+		currentTime: (jsPlayer.querySelector('.current-time') || {}),
+		durationTime: (jsPlayer.querySelector('.duration-time') || {}),
+		seekBar: (jsPlayer.querySelector('.seek-bar') || { style: {} }),
+		bigPlayButton: (jsPlayer.querySelector('.big-play-button') || { style: {} }),
+		bigPauseButton: (jsPlayer.querySelector('.big-pause-button') || { style: {} }),
+		playButton: (jsPlayer.querySelector('.play-button') || { style: {} }),
+		pauseButton: (jsPlayer.querySelector('.pause-button') || { style: {} }),
+		prevButton: (jsPlayer.querySelector('.prev-button') || { style: {} }),
+		nextButton: (jsPlayer.querySelector('.next-button') || { style: {} }),
+		playlistButton: (jsPlayer.querySelector('.playlist-button') || { style: {} }),
+		titleText: (jsPlayer.querySelector('.title-text') || { style: {} }),
+		artistText: (jsPlayer.querySelector('.artist-text') || { style: {} }),
+    seekInterval: null,
+		trackCount: 0,
+		playing: false,
+    playlist: [],
+		tracks: [],
+		idx: 0
+	};
 
-$(document).ready(function(event) {
-  window.e1 = new Equalizer("eq1", 75, 200, 0.8, 0.2, 3, 2, "#dadfe1");
-  window.e1.start();
-})
-
-var Equalizer = function(_canvas, _numBars, _height, _barRatio, _paddingRatio, _growSpeed, _shrinkSpeed, _color) {
-
-  //---------------------------------------
-  // Private Variables
-  //---------------------------------------
-
-  if (_barRatio > 1 || _paddingRatio > 1) {
-    console.error("There was a problem with the ratio value provided. They must be less than or equal to 1. Given Values:", _barRatio, _paddingRatio);
-    return;
-  }
-
-  var animating = false;
-
-  var midPoint = 0;
-
-  var maxBarHeight = _height;
-  var maxBarSmall = 50;
-  var minBarHeight = 20;
-
-  var barRatio = _barRatio;
-  var paddingRatio = _paddingRatio;
-
-  var barPadding = 10;
-  var barWidth = 10;
-
-  var totalBars = _numBars;
-
-  var barColor = _color;
-
-  var canvas = document.getElementById(_canvas);
-  var ctx = canvas.getContext("2d");
-
-  var bar = [];
-
-  var width = canvas.width;
-
-  var speed = {
-    grow: _growSpeed,
-    shrink: _shrinkSpeed
-  };
-
-  // var mousePos = {
-  //   x: 0,
-  //   y: 0
-  // };
-
-  var resizeTimeOut = false;
-
-  //-----------------------------
-  // Bind function calls to dom
-  //-----------------------------
-  init();
-
-  //-----------------------------
-  // init() 
-  // - Begins the animation sequence 
-  //-----------------------------
-  function init() {
-    initBars();
-    bindResizeEvents();
-    // bindMouseMove();
-  }
-
-  function start() {
-    animating = true;
-    update();
-  }
-
-  function initBars() {
-    resize($(window).width());
-    var barHeight = 0,
-      x = 0,
-      y = 0;
-
-    midpoint = width / 2;
-
-    for (var i = 0; i < totalBars; i++) {
-      x = (i + i) * barPadding + (barWidth * i);
-
-      barHeight = randomizeHeight(x);
-      y = getHeight(barHeight);
-
-      bar.push({
-        height: barHeight,
-        buffHeight: barHeight,
-        direction: -1,
-        x: x,
-        y: y
-      });
-    }
-  }
-
-  //-----------------------------
-  // bindResizeEvents() 
-  // - used to bind resizeEvents
-  //-----------------------------
-  function bindResizeEvents() {
-    $(window).on('resize', function(e) {
-      if (resizeTimeOut !== false)
-        clearTimeout(resizeTimeOut);
-
-      resizeTimeOut = setTimeout(function() {
-        resize($(this).width());
-      }, 200);
-    });
-  }
-
-  //-----------------------------
-  // stop() 
-  // - Stops the animation sequence 
-  //-----------------------------
-  // function stop() {
-  //   animating = false;
-  // }
-
-  //-----------------------------
-  // draw() 
-  // - Draw the contents of an individual frame 
-  //-----------------------------
-  function draw() {
-    for (var index = 0; index < bar.length; index++) {
-
-      ctx.fillStyle = barColor;
-      ctx.clearRect(bar[index].x, bar[index].y, barWidth, bar[index].height);
-      bar[index].y = getHeight(bar[index].height);
-      bar[index].x = (index + index) * barPadding + (barWidth * index);
-
-//       if (mousePos.x >= bar[index].x - barWidth * 10 &&
-//         mousePos.x < bar[index].x + barWidth * 10 &&
-//         Math.abs(mousePos.y - maxBarHeight / 2) < 100) {
-//         bar[index].direction = -1;
-
-//       }
-
-      if (bar[index].height < bar[index].buffHeight && bar[index].direction > 0) {
-        bar[index].height += speed.grow;
-      } else if (bar[index].direction > 0) {
-        bar[index].direction = -1;
-      }
-
-      // If increasing grow it 
-      if (bar[index].height > bar[index].buffHeight / 1.25 && bar[index].direction < 0) {
-        bar[index].height -= speed.shrink;
-      } else if (bar[index].direction < 0) {
-        bar[index].buffHeight = randomizeHeight(bar[index].x);
-        bar[index].direction = 1;
-      }
-
-      ctx.fillRect(bar[index].x, bar[index].y, barWidth, bar[index].height);
-    }
-
-    var grd = ctx.createLinearGradient(146.000, 0.000, 154.000, 4000.000);
-    /*         var grd = ctx.createLinearGradient(146.000, 0.000, 154.000, 600.000);*/
-
-    // Add colors
-    grd.addColorStop(0.000, 'rgba(255, 255, 255, 0.000)');
-    grd.addColorStop(1.000, 'rgba(0, 0, 0, 1.000)');
-
-    // Fill with gradient
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 400, width, 400);
-
-  }
-
-  //-----------------------------
-  // update() 
-  // - Redraws Canvas each frame
-  //-----------------------------
-  function update() {
-    if (!animating) return;
-    draw();
-    requestAnimationFrame(update);
-  }
-
-  //-----------------------------
-  // getHeight() 
-  // - Returns the height in relation to the maxBarHeight
-  //-----------------------------
-  function getHeight(_height) {
-    return Math.round((maxBarHeight - _height) / 2);
-  }
-
-  //-----------------------------
-  // bindMouseMove() 
-  // - bind the mouse move event to the canvas
-  //-----------------------------
-  // function bindMouseMove() {
-  //   canvas.addEventListener('mousemove', function(e) {
-  //     mousePos = getMousePos(e);
-  //   });
-  // }
-
-  //-----------------------------
-  // getMousePos() 
-  // - Return the mouse position
-  //-----------------------------
-  // function getMousePos(e) {
-  //   var rect = canvas.getBoundingClientRect();
-  //   return {
-  //     x: e.clientX - rect.left,
-  //     y: e.clientY - rect.top
-  //   };
-  // }
-
-  //-----------------------------
-  // randomizeHeight() 
-  // - Calculate a new maxheight to reach when increasing in size
-  //-----------------------------
-  function randomizeHeight(pos) {
-    var maxVal,
-      temp;
-
-    if (pos > midPoint) {
-      pos = pos - midPoint;
-      temp = Math.abs((pos / midPoint) - 1);
+	jsPlayer.playClicked = function jsPlayerPlayClicked(){
+		jsPlayer.bigPauseButton.style.display = 'block';
+		jsPlayer.bigPlayButton.style.display = 'none';
+		jsPlayer.pauseButton.style.display = 'block';
+		jsPlayer.playButton.style.display = 'none';
+		jsPlayer.playing = true;
+		jsPlayer.player.play();
+		jsPlayer.seekInterval = setInterval(jsPlayer.updateSeek, 500);
+	};
+	jsPlayer.pauseClicked = function jsPlayerPauseClicked(){
+		clearInterval(jsPlayer.seekInterval);
+		jsPlayer.bigPlayButton.style.display = 'block';
+		jsPlayer.bigPauseButton.style.display = 'none';
+		jsPlayer.playButton.style.display = 'block';
+		jsPlayer.pauseButton.style.display = 'none';
+		jsPlayer.playing = false;
+		jsPlayer.player.pause();
+	};
+  jsPlayer.mediaEnded = function jsPlayerMediaEnded(){
+    if (jsPlayer.idx + 1 < jsPlayer.trackCount) {
+      jsPlayer.idx++;
+      jsPlayer.playTrack(jsPlayer.idx);
     } else {
-      temp = (pos / midPoint);
-      if (temp == 0) temp = 0.01;
+      jsPlayer.pauseClicked();
+      jsPlayer.idx = 0;
+      jsPlayer.loadTrack(jsPlayer.idx);
     }
-
-    maxVal = temp * maxBarHeight;
-    return Math.floor((Math.random() * maxVal) + minBarHeight);
-  }
-
-  //-----------------------------
-  // resize() 
-  // - Handles resizing the canvas elements according to a specified canvas width
-  //-----------------------------
-  function resize(_width) {
-    width = _width;
-    canvas.width = _width;
-    midPoint = _width / 2;
-    totalWidth = Math.abs(_width / totalBars);
-    barWidth = Math.ceil(totalWidth * barRatio);
-    barPadding = Math.ceil(totalWidth * paddingRatio);
-  }
-
-  //-----------------------------
-  // setSpeed() 
-  // - set speed values used to grow or shrink the size 
-  //-----------------------------
-  function setSpeed(_grow, _shrink) {
-    speed.grow = _grow;
-    speed.shrink = _shrink;
-  }
-
-  //---------------------------------------
-  // Publicly Accessible Variables / Functions 
-  //---------------------------------------
-  return ({
-    start: start,
-    stop: stop
-  });
+  };
+	jsPlayer.loadTracklist = function jsPlayerLoadPlaylist(){
+		jsPlayer.playlist = jsPlayer.wrapList.tagName ? jsPlayer.wrapList.querySelectorAll('ol > li') : [];
+		var len = jsPlayer.playlist.length,
+			tmp, i;
+    if (len > 0) {
+      jsPlayer.wrap.classList.add('list-view');
+      for (i = jsPlayer.trackCount; i < len; i++) {
+        if (!jsPlayer.playlist[i].dataset) {
+          jsPlayer.playlist[i].dataset = {};
+        }
+        tmp = jsPlayer.playlist[i].querySelector('a');
+        if (tmp.tagName && !jsPlayer.playlist[i].dataset.idx) {
+          jsPlayer.playlist[i].dataset.idx = i;
+          jsPlayer.trackCount++;
+          jsPlayer.tracks.push({
+            'file': tmp.href,
+            'artist': tmp.dataset.artist ? 'by ' + decodeURIComponent(tmp.dataset.artist).replace(/^\s+|\s+$/g, '') : '&nbsp;',
+            'name': decodeURIComponent(tmp.textContent || tmp.innerText).replace(/^\s+|\s+$/g, '')
+          });
+        }
+      }
+    }
+	};
+	jsPlayer.loadTrack = function jsPlayerLoadTrack(idx){
+		var len = jsPlayer.playlist ? jsPlayer.playlist.length : 0,
+			i;
+		for (i=0; i < len; i++) {
+			if (jsPlayer.playlist[i].classList) {
+				if (i == idx) {
+					jsPlayer.playlist[i].classList.add('sel');
+				} else {
+					jsPlayer.playlist[i].classList.remove('sel');
+				}
+			}
+		}
+		jsPlayer.titleText.innerHTML = jsPlayer.tracks[idx].name;
+		jsPlayer.artistText.innerHTML = jsPlayer.tracks[idx].artist;
+		jsPlayer.player.src = jsPlayer.tracks[idx].file;
+    jsPlayer.idx = idx;
+	};
+	jsPlayer.playTrack = function jsPlayerPlayTrack(idx){
+		jsPlayer.loadTrack(idx);
+		jsPlayer.playClicked();
+	};
+  jsPlayer.listClicked = function jsPlayerListClicked(event){
+    clearInterval(jsPlayer.seekInterval);
+    var parent = event.target.parentNode;
+    if (parent.parentNode.tagName.toLowerCase() === 'ol') {
+      event.preventDefault();
+      jsPlayer.playTrack(parent.dataset.idx);
+    }
+  };
+  jsPlayer.setDuration = function jsPlayerSetDuration(){
+    jsPlayer.durationTime.innerHTML = jsPlayer.formatTime(jsPlayer.player.duration);
+    jsPlayer.currentTime.innerHTML = jsPlayer.formatTime(jsPlayer.player.currentTime);
+    jsPlayer.seekBar.value = jsPlayer.player.currentTime / jsPlayer.player.duration;
+  };
+  jsPlayer.updateSeek = function jsPlayerUpdateSeek(){
+    if (jsPlayer.player.duration > -1) {
+      jsPlayer.seekBar.value = 100 * (jsPlayer.player.currentTime || 0) / jsPlayer.player.duration;
+      jsPlayer.currentTime.innerHTML = jsPlayer.formatTime(jsPlayer.player.currentTime || 0);
+    }
+  };
+  jsPlayer.seekHeld = function jsPlayerSeekHeld(){
+    jsPlayer.seekBar.parentNode.classList.add('sel');
+    clearInterval(jsPlayer.seekInterval);
+    jsPlayer.player.pause();
+  };
+  jsPlayer.seekReleased = function jsPlayerSeekReleased(){
+    if (jsPlayer.player.duration > -1) {
+      jsPlayer.player.currentTime = jsPlayer.seekBar.value * jsPlayer.player.duration / 100;
+      jsPlayer.seekBar.parentNode.classList.remove('sel');
+      if (jsPlayer.playing) {
+        jsPlayer.player.play();
+        jsPlayer.seekInterval = setInterval(jsPlayer.updateSeek, 500);
+      } else {
+        jsPlayer.updateSeek();
+      }
+    }
+  };
+  jsPlayer.prevClicked = function jsPlayerPrevClicked(event){
+    event.preventDefault();
+    if (jsPlayer.idx - 1 > -1) {
+      jsPlayer.idx--;
+      if (jsPlayer.playing) {
+        jsPlayer.playTrack(jsPlayer.idx);
+      } else {
+        jsPlayer.loadTrack(jsPlayer.idx);
+      }
+    } else {
+      jsPlayer.pauseClicked();
+      jsPlayer.idx = 0;
+      jsPlayer.loadTrack(jsPlayer.idx);
+    }
+  };
+  jsPlayer.nextClicked = function jsPlayerNextClicked(event){
+    event.preventDefault();
+    if (jsPlayer.idx + 1 < jsPlayer.trackCount) {
+      jsPlayer.idx++;
+      if (jsPlayer.playing) {
+        jsPlayer.playTrack(jsPlayer.idx);
+      } else {
+        jsPlayer.loadTrack(jsPlayer.idx);
+      }
+    } else {
+      jsPlayer.pauseClicked();
+      jsPlayer.idx = 0;
+      jsPlayer.loadTrack(jsPlayer.idx);
+    }
+  };
+  jsPlayer.playlistButtonClicked = function jsPlayerPlaylistButtonClicked(){
+    jsPlayer.wrap.classList.toggle('show-list');
+    jsPlayer.playlistButton.style.backgroundImage = (jsPlayer.wrap.classList.contains('show-list') && jsPlayer.wrap.style.backgroundImage) ? jsPlayer.wrap.style.backgroundImage : '';
+  };
+  jsPlayer.formatTime = function jsPlayerFormatTime(val){
+    var h = 0, m = 0, s;
+    val = (parseInt(val, 10) || 0);
+    if (val > 60 * 60) {
+      h = parseInt(val / (60 * 60), 10);
+      val -= h * 60 * 60;
+    }
+    if (val > 60) {
+      m = parseInt(val / 60, 10);
+      val -= m * 60;
+    }
+    s = val;
+    val = (h > 0)? h + ':' : '';
+    val += (m > 0)? ((m < 10 && h > 0)? '0' : '') + m + ':' : '0:';
+    val += ((s < 10)? '0' : '') + s;
+    return val;
+  };
+	jsPlayer.init = function jsPlayerInit(){
+		if (!!document.createElement('audio').canPlayType('audio/mpeg')) {
+			if (jsPlayer.wrapList.tagName && jsPlayer.wrapList.querySelectorAll('ol > li').length > 0) {
+				jsPlayer.loadTracklist();
+			} else if (jsPlayer.wrap.tagName && jsPlayer.wrap.dataset.url) {
+				jsPlayer.tracks = [{
+					'file': jsPlayer.wrap.dataset.url,
+          'artist': 'by-' + decodeURIComponent(jsPlayer.wrap.dataset.artist || 'unknown').replace(/^\s+|\s+$/g, ''),
+					'name': decodeURIComponent(jsPlayer.wrap.dataset.title || '').replace(/^\s+|\s+$/g, '')
+				}];
+			}
+			if (jsPlayer.tracks.length > 0 && jsPlayer.player) {
+        jsPlayer.player.addEventListener('ended', jsPlayer.mediaEnded, true);
+        jsPlayer.player.addEventListener('loadeddata', jsPlayer.setDuration, true);
+				if (jsPlayer.wrapList.tagName) {
+					jsPlayer.wrapList.addEventListener('click', jsPlayer.listClicked, true);
+				}
+				if (jsPlayer.bigPlayButton.tagName) {
+					jsPlayer.bigPlayButton.addEventListener('click', jsPlayer.playClicked, true);
+          if (jsPlayer.bigPauseButton.tagName) {
+            jsPlayer.bigPauseButton.addEventListener('click', jsPlayer.pauseClicked, true);
+          }
+				}
+				if (jsPlayer.playButton.tagName) {
+					jsPlayer.playButton.addEventListener('click', jsPlayer.playClicked, true);
+          if (jsPlayer.pauseButton.tagName) {
+            jsPlayer.pauseButton.addEventListener('click', jsPlayer.pauseClicked, true);
+          }
+				}
+				if (jsPlayer.prevButton.tagName) {
+					jsPlayer.prevButton.addEventListener('click', jsPlayer.prevClicked, true);
+				}
+				if (jsPlayer.nextButton.tagName) {
+					jsPlayer.nextButton.addEventListener('click', jsPlayer.nextClicked, true);
+				}
+				if (jsPlayer.playlistButton.tagName) {
+					jsPlayer.playlistButton.addEventListener('click', jsPlayer.playlistButtonClicked, true);
+				}
+				if (jsPlayer.seekBar.tagName) {
+					jsPlayer.seekBar.addEventListener('mousedown', jsPlayer.seekHeld, true);
+					jsPlayer.seekBar.addEventListener('mouseup', jsPlayer.seekReleased, true);
+				}
+        jsPlayer.wrap.className += ' enabled';
+        jsPlayer.loadTrack(jsPlayer.idx);
+			}
+		}
+	};
+	jsPlayer.init();
 }
